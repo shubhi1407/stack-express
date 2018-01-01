4159809cf9558f40a78038c1fb3415c535babf46
Shubhangi Gupta <shubhangi140793@gmail.com>
2017-12-31 18:49:08 -0700

    modified creation date

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var express = require('express');
 var _ = require('underscore');
-var cors= require('cors');
+var cors = require('cors');
 var countries = require('./countries.json');
 
 var app = express();
@@ -9,35 +9,38 @@ app.use(cors());
 app.use(express.static('public'))
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "admin",
-  password: "password",
-  database : 'stackNetwork'
+    host: "localhost",
+    user: "admin",
+    password: "password",
+    database: 'stackNetwork'
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+    if (err) throw err;
+    console.log("Connected!");
 });
 
-app.get('/getColumns', function(req, res){
-	con.query('select * from demographic where DATE(creation_date)=(select DATE(MAX(creation_date)) from demographic)', function(error,results){
+app.get('/getColumns', function(req, res) {
+    con.query('select * from demographic where DATE(creation_date)=(select DATE(MAX(creation_date)) from demographic)', function(error, results) {
+        var creationDate = results[0].creation_date;
+        if (error) throw error;
+        var response = {};
+        _.each(results, function(res) {
+            var country = _.find(countries, function(country) {
+                return country.name == res.Location;
 
-			if(error) throw error;
-			var response = {};
-			_.each(results,function(res){
-				var country =_.find(countries,function(country){
-					return country.name==res.Location;
+            })
+            if (country != null && country != undefined) {
+                response[country.code] = res.count;
+            }
+        })
 
-				})
-				if(country!=null && country!=undefined){
-					response[country.code]=res.count;
-				} 
-			})
+        res.json({
+            creation_Date: creationDate,
+            result: response
+        });
+    })
 
-			res.json(response); 	 	
-	})
-  
 });
 
 app.listen(3001);
